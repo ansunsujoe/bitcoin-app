ef7e1c0ba65c82e86c4bb4bc7465eaca3bdf6419
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2021-11-12 12:42:12 -0600

    Start backend model

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bitcoin-app
 Bitcoin Application for CS 6360
+
+## Backend
+A Flask app is used to connect to MySQL
+Details here: https://www.section.io/engineering-education/flask-database-integration-with-sqlalchemy/
--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask import request, session
 import os
+from flask_sqlalchemy import SQLAlchemy
 
 # Flask App config setup
 app = Flask(__name__)
@@ -9,6 +10,10 @@ app.config['ENV'] = "development"
 app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
 app.secret_key = os.environ.get("SECRET_KEY")
 CORS(app, supports_credentials=True)
+
+# Flask Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/bitcoin'
+db = SQLAlchemy(app)
 
 # Root endpoint
 @app.route("/")
