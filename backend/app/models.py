3dbbc059d5a768b68b2c38a986a4ffce3d0524fd
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2021-11-22 21:48:08 -0600

    Data models and dbs

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,8 +1,40 @@
 from app import db
 
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    phone_number = db.Column(db.String)
+    cell = db.Column(db.String)
+    email = db.Column(db.String)
+    street_address = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    zip = db.Column(db.String)
+    password = db.Column(db.String)
+    is_trader = db.Column(db.Boolean)
+    is_manager = db.Column(db.Boolean)
+
 class Transaction(db.Model):
     transaction_id = db.Column(db.Integer, primary_key=True)
-    trader_id = db.Column(db.Integer)
+    trader_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    client_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    commission_type = db.Column(db.String)
+    status = db.Column(db.String)
+    date = db.Column(db.DateTime)
+    currency_type = db.Column(db.String)
+    amount = db.Column(db.Float)
+
+class Client(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id), primary_key=True)
+    fiat_balance = db.Column(db.Float)
+    btc_balance = db.Column(db.Float)
+    user_classification = db.Column(db.String)
+
+class Processed(db.Model):
+    order_id = db.Column(db.Integer, primary_key=True)
+    trader_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    transaction_id = db.Column(db.Integer, db.ForeignKey(Transaction.transaction_id))
+    commission_paid = db.Column(db.Float)
     
-class User(db.Model):
+class Issues_On_Behalf(db.Model):
     pass
---
+++ database/bitcoin.sql
@@ -0,0 +1,61 @@
+-- User Table
+CREATE TABLE IF NOT EXISTS User (
+    User_ID INT NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    Phone_number VARCHAR(15) NOT NULL,
+    Cell VARCHAR(15) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    Street_Address VARCHAR(50) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    State VARCHAR(20) NOT NULL,
+    Zip VARCHAR(5) NOT NULL,
+    Password VARCHAR(50) NOT NULL,
+    Is_Trader BOOLEAN NOT NULL,
+    Is_Manager BOOLEAN NOT NULL,
+    PRIMARY KEY (User_ID)
+);
+
+-- Transaction Table
+CREATE TABLE IF NOT EXISTS Transaction (
+	Transaction_id int NOT NULL,
+    Trader_id int NOT NULL,
+	Commission_type VARCHAR(3),
+	Client_id NOT NULL,
+	Status VARCHAR(10),
+    Date DATE,
+    Currency_type VARCHAR(10),
+	Amount DOUBLE(32, 10),
+	PRIMARY KEY (Transaction_id),
+	FOREIGN KEY (Client_id) REFERENCES User(User_ID),
+    FOREIGN KEY (Trader_id) REFERENCES User(User_ID)
+);
+
+-- Client table
+CREATE TABLE IF NOT EXISTS Client (
+    User_ID INT NOT NULL,
+    Fiat_balance DOUBLE(32, 2) NOT NULL,
+    BTC_balance DOUBLE(32, 10) NOT NULL,
+    User_classification VARCHAR(10) NOT NULL,
+    PRIMARY KEY (User_ID),
+    FOREIGN KEY (User_ID) REFERENCES User(User_ID)
+);
+
+-- Processed table
+CREATE TABLE IF NOT EXISTS Processed (
+	Trader_id INT,
+    Transaction_id INT,
+	Order_id INT,
+	Commission_paid DOUBLE(32, 10),
+    PRIMARY KEY (Order_id),
+    FOREIGN KEY (Trader_id) REFERENCES User(User_ID),
+    FOREIGN KEY (Transaction_id) REFERENCES Transaction(Transaction_id)
+);
+
+-- Issues table
+CREATE TABLE IF NOT EXISTS Issues_On_Behalf (
+    Transaction_id INT,
+	Trader_id INT,
+	Client_id INT,
+	PRIMARY KEY(Transaction_id)
+	Foreign key (Client_id, Trader_id) REFERENCES User
+);
