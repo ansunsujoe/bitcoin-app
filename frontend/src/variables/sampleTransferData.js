ba84c35cca211f63cae346de8f4cf0aa5dc83e2f
Ashish P <26324100+alphayama@users.noreply.github.com>
2021-11-23 16:16:13 -0600

    adding sample transfer data

---
+++ frontend/src/variables/sampleTransferData.js
@@ -0,0 +1,44 @@
+const transferData = [
+    {
+        "amount": "36738",
+        "time": "2019-5-19 2:55:24",
+        "client": "Dakota Rice",
+        "commission": "BTC",
+        "status": "Complete",
+        
+    },
+    {
+        "amount": "1413",
+        "time": "2019-5-19 2:55:24",
+        "client": "Dakota Rice",
+        "commission": "USD",
+        "status": "Pending",
+        
+    },
+    {
+        "amount": "23",
+        "time": "2019-5-19 2:55:24",
+        "client": "Dakota Rice",
+        "commission": "BTC",
+        "status": "Pending",
+        
+    },
+    {
+        "amount": "364",
+        "time": "2019-5-19 2:55:24",
+        "client": "Dakota Rice",
+        "commission": "USD",
+        "status": "Complete",
+        
+    },
+    {
+        "amount": "289",
+        "time": "2019-5-19 2:55:24",
+        "client": "Dakota Rice",
+        "commission": "USD",
+        "status": "Pending",
+        
+    },
+]
+
+export {transactionData};
