0594fa872291964e1a6c855bef01a72cb78d4f10
Ansun Sujoe <ansunsujoe@gmail.com>
2021-11-28 07:43:17 -0600

    Update users.py

--- backend/app/users.py
+++ backend/app/users.py
@@ -14,10 +14,66 @@ def get_users():
     
 
 # Modify/delete user
-@app.route("/users/<user_id>", methods=["GET"])
+@app.route("/users/<user_id>", methods=["GET", "PUT", "DELETE"])
 def user_info(user_id):
-        user = db.session.query(User).filter(User.user_id == user_id).first()
-        return to_json(user)
+    if request.method == "GET":
+        # User client query
+        user, client = db.session.query(
+            User, Client
+        ).filter(
+            User.user_id == user_id
+        ).filter(
+            User.user_id == Client.user_id
+        ).first()
+
+        if user is not None:
+            client_json = {
+                "id": user.user_id,
+                "name": user.name,
+                "btcBalance": round(client.btc_balance, 1),
+                "fiatBalance": round(client.fiat_balance, 2),
+                "classification": client.user_classification,
+                "isClient": True,
+                "isTrader": user.is_trader,
+                "isManager": user.is_manager,
+                "phoneNumber": user.phone_number,
+                "cell": user.cell,
+                "email": user.email,
+                "streetAddress": user.street_address,
+                "city": user.city,
+                "state": user.state,
+                "zip": user.zip
+            }
+            return client_json
+        
+        # User query
+        user = db.session.query(
+            User
+        ).filter(
+            User.user_id == user_id
+        ).first()
+        
+        # User json
+        user_json = {
+            "id": user.user_id,
+            "name": user.name,
+            "isClient": False,
+            "isTrader": user.is_trader,
+            "isManager": user.is_manager,
+            "phoneNumber": user.phone_number,
+            "cell": user.cell,
+            "email": user.email,
+            "streetAddress": user.street_address,
+            "city": user.city,
+            "state": user.state,
+            "zip": user.zip
+        }
+        return user_json
+    
+    elif request.method == "PUT":
+        pass
+    elif request.method == "DELETE":
+        pass
    
     
 @app.route("/users/clients/<client_id>", methods=["GET"])
