191cc56b88257812222346c3deb5940e1d6280c9
Ashish Pratap <26324100+alphayama@users.noreply.github.com>
2021-11-27 18:32:33 -0600

    updating NewTransfer.js

--- frontend/src/views/NewTransfer.js
+++ frontend/src/views/NewTransfer.js
@@ -31,47 +31,46 @@ import {
     Row,
     Col,
 } from "reactstrap";
-import Slider from '@mui/material/Slider';
-import { getCommission, roundDecimal } from '../utilities/transaction';
+// import Slider from '@mui/material/Slider';
+// import { getCommission, roundDecimal } from '../utilities/transaction';
 import axios from 'axios';
 
-function NewTransaction(props) {
-    const [buyTrader, setBuyTrader] = useState("trader-1");
-    const [sellTrader, setSellTrader] = useState("trader-2");
-    const [buyCommissionType, setBuyCommissionType] = useState("USD");
-    const [sellCommissionType, setSellCommissionType] = useState("USD");
-    const [buyCommission, setBuyCommission] = useState("0");
-    const [sellCommission, setSellCommission] = useState("0");
-    const [buyCost, setBuyCost] = useState(0.0);
-    const [sellCost, setSellCost] = useState(0.0);
-    const [buyDisabled, setBuyDisabled] = useState(true);
-    const [sellDisabled, setSellDisabled] = useState(true);
-    const [buyAmount, setBuyAmount] = useState(0);
-    const [sellAmount, setSellAmount] = useState(0);
-    const [btcRate, setBtcRate] = useState(50000.0);
+function NewTransfer(props) {
+    const [transferTrader, setTransferTrader] = useState("trader-1");
+    //const [sellTrader, setSellTrader] = useState("trader-2");
+    // const [buyCommissionType, setBuyCommissionType] = useState("USD");
+    // const [sellCommissionType, setSellCommissionType] = useState("USD");
+    // const [buyCommission, setBuyCommission] = useState("0");
+    // const [sellCommission, setSellCommission] = useState("0");
+    // const [buyCost, setBuyCost] = useState(0.0);
+    // const [sellCost, setSellCost] = useState(0.0);
+    const [transferDisabled, setTransferDisabled] = useState(true);
+    // const [sellDisabled, setSellDisabled] = useState(true);
+    const [transferAmount, setTransferAmount] = useState(0);
+    // const [sellAmount, setSellAmount] = useState(0);
+    // const [btcRate, setBtcRate] = useState(0.0)
     const [traderList, setTraderList] = useState([]);
     const [clientProperties, setClientProperties] = useState({});
 
     axios.defaults.withCredentials = true;
 
-    // Get current Bitcoin price
-    const getCurrentBTC = () => {
-        axios.get('http://localhost:5000/btc-rate')
-            .then(response => {
-                console.log(response.data);
-                setBtcRate(response.data.results);
-            }).catch(error => {
-                console.log(error);
-            })
-    }
+    //   // Get current Bitcoin price
+    //   const getCurrentBTC = () => {
+    //     axios.get('http://localhost:5000/btc-rate')
+    //       .then(response => {
+    //         setBtcRate(response.data)
+    //       }).catch(error => {
+    //         console.log(error);
+    //       })
+    //   }
 
     // Get trader list
     const getTraderList = () => {
         axios.get('http://localhost:5000/users/traders')
             .then(response => {
                 setTraderList(response.data.results);
-                setBuyTrader(response.data.results[0].id);
-                setSellTrader(response.data.results[0].id);
+                setTransferTrader(response.data.results[0].id);
+                //setSellTrader(response.data.results[0].id);
             }).catch(error => {
                 console.log(error);
             })
@@ -88,54 +87,54 @@ function NewTransaction(props) {
             })
     }
 
-    // Get current bitcoin price immediately
-    useEffect(() => {
-        getCurrentBTC();
-        getTraderList();
-        getUserData();
-    }, []);
+    // // Get current bitcoin price immediately
+    // useEffect(() => {
+    //     //getCurrentBTC();
+    //     getTraderList();
+    //     getUserData();
+    // }, []);
 
-    // Get current bitcoin price every 10 seconds
-    useEffect(() => {
-        const interval = setInterval(() => {
-            getCurrentBTC();
-        }, 10000);
-        return () => clearInterval(interval);
-    }, []);
+    //   // Get current bitcoin price every 10 seconds
+    //   useEffect(() => {
+    //     const interval = setInterval(() => {
+    //       getCurrentBTC();
+    //     }, 10000);
+    //     return () => clearInterval(interval);
+    //   }, []);
 
-    // Change other variables when BTC rate changes
-    useEffect(() => {
-        setBuyCommission(getCommission(btcRate, buyAmount, buyCommissionType, "silver"));
-        setSellCommission(getCommission(btcRate, sellAmount, sellCommissionType, "silver"));
-        setBuyCost(buyAmount > 0 ? btcRate * buyAmount + buyCommission : 0.00);
-        setSellCost(sellAmount > 0 ? btcRate * sellAmount - sellCommission : 0.00);
-    }, [btcRate, buyAmount, sellAmount]);
+    //   // Change other variables when BTC rate changes
+    //   useEffect(() => {
+    //     setBuyCommission(getCommission(btcRate, buyAmount, buyCommissionType, "silver"));
+    //     setSellCommission(getCommission(btcRate, sellAmount, sellCommissionType, "silver"));
+    //     setBuyCost(buyAmount > 0 ? btcRate * buyAmount + buyCommission : 0.00);
+    //     setSellCost(sellAmount > 0 ? btcRate * sellAmount - sellCommission : 0.00);
+    //   }, [btcRate, buyAmount, sellAmount]);
 
-    // Handler functions for buy slider
-    const handleBuyChange = (e, val) => {
-        setBuyAmount(val);
+    // Handler functions for transfer slider
+    const handleTransferChange = (e, val) => {
+        setTransferAmount(val);
         if (val === 0) {
-            setBuyDisabled(true);
+            setTransferDisabled(true);
         }
         else {
-            setBuyDisabled(false);
+            setTransferDisabled(false);
         }
     }
 
-    // Handler functions for sell slider
-    const handleSellChange = (e, val) => {
-        setSellAmount(val);
-        if (val === 0) {
-            setSellDisabled(true);
-        }
-        else {
-            setSellDisabled(false);
-        }
-    }
+    // // Handler functions for sell slider
+    // const handleSellChange = (e, val) => {
+    //     setSellAmount(val);
+    //     if (val === 0) {
+    //         setSellDisabled(true);
+    //     }
+    //     else {
+    //         setSellDisabled(false);
+    //     }
+    // }
 
     // Buy/Sell Axios Request
-    const transactionSubmit = (data) => {
-        axios.post('http://localhost:5000/users/' + props.userId + '/transactions', data, {
+    const transferSubmit = (data) => {
+        axios.post('http://localhost:5000/users/' + props.userId + '/transfers', data, {
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -147,26 +146,26 @@ function NewTransaction(props) {
     }
 
     // Handler function for submitting buy
-    const handleBuySubmit = (e) => {
+    const handleTransferSubmit = (e) => {
         const data = {
-            commission_type: buyCommissionType,
-            amount: buyAmount,
-            traderId: buyTrader,
+            //commission_type: buyCommissionType,
+            amount: transferAmount,
+            traderId: transferTrader,
             action: "buy"
         };
-        transactionSubmit(data);
+        transferSubmit(data);
     }
 
-    // Handler function for submitting sell
-    const handleSellSubmit = (e) => {
-        const data = {
-            commission_type: sellCommissionType,
-            amount: sellAmount,
-            traderId: sellTrader,
-            action: "sell"
-        };
-        transactionSubmit(data);
-    }
+    // // Handler function for submitting sell
+    // const handleSellSubmit = (e) => {
+    //     const data = {
+    //         commission_type: sellCommissionType,
+    //         amount: sellAmount,
+    //         traderId: sellTrader,
+    //         action: "sell"
+    //     };
+    //     transactionSubmit(data);
+    // }
 
     return (
         <>
@@ -175,53 +174,40 @@ function NewTransaction(props) {
                     <Col md="12">
                         <Card className="card-user">
                             <CardHeader>
-                                <CardTitle tag="h5">Buy Transaction Form</CardTitle>
+                                <CardTitle tag="h5">Transfer Money</CardTitle>
                             </CardHeader>
                             <CardBody>
-                                <Form onSubmit={handleBuySubmit}>
+                                <Form onSubmit={handleTransferSubmit}>
                                     <Row>
-                                        <Col className="pr-1" md="3">
-                                            <FormGroup>
-                                                <label>Commission Type</label>
-                                                <Input type="select" name="commissionType" id="commissionType"
-                                                    value={buyCommissionType} onChange={e => setBuyCommissionType(e.currentTarget.value)}>
-                                                    <option>USD</option>
-                                                    <option>BTC</option>
-                                                </Input>
-                                            </FormGroup>
-                                        </Col>
-                                        <Col className="px-1" md="3">
+                                        <Col className="px-1" md="4">
                                             <FormGroup>
                                                 <label>Assign Trader</label>
                                                 <Input type="select" name="transactionType" id="transactionType"
-                                                    value={buyTrader} onChange={e => setBuyTrader(e.currentTarget.value)}>
+                                                    value={transferTrader} onChange={e => setTransferTrader(e.currentTarget.value)}>
                                                     {traderList.map((t) => (
                                                         <option key={t.id} value={t.id}>{t.name}</option>
                                                     ))}
                                                 </Input>
                                             </FormGroup>
                                         </Col>
-                                        <Col className="px-1" md="3">
+                                        <Col className="px-1" md="4">
                                             <FormGroup>
-                                                <label htmlFor="exampleInputEmail1">
-                                                    Commission
-                                                </label>
-                                                <Input placeholder={buyCommissionType === "BTC" ? buyCommission + " \u20BF" : "$" + buyCommission} disabled />
+                                                <label>Enter Amount:</label>
+                                                <Input
+                                                    defaultValue={0}
+                                                    type="number"
+                                                    step={0.01}
+                                                    min={0}
+                                                    onChange={handleTransferChange}
+                                            />
                                             </FormGroup>
                                         </Col>
-                                        <Col className="pl-1" md="3">
-                                            <FormGroup>
-                                                <label htmlFor="exampleInputEmail1">
-                                                    Current Cost
-                                                </label>
-                                                <Input placeholder={"$" + buyCost} disabled />
-                                            </FormGroup>
-                                        </Col>
+                                        
                                     </Row>
-                                    <Row>
+                                    {/* <Row>
                                         <Col md="12">
                                             <FormGroup className="mt-4 ml-3 mr-1">
-
+                                                <label>Choose amount: (Max. USD 20,000)</label>
                                                 <Slider
                                                     disabled={false}
                                                     defaultValue={0}
@@ -229,104 +215,22 @@ function NewTransaction(props) {
                                                     step={0.1}
                                                     marks
                                                     min={0}
-                                                    max={btcRate > 0 ? roundDecimal(clientProperties.fiatBalance / btcRate, 1, true) : 0.0}
+                                                    max={20000}
                                                     valueLabelDisplay="on"
                                                     color="success"
-                                                    onChange={handleBuyChange}
+                                                    onChange={handleTransferChange}
                                                 />
                                             </FormGroup>
                                         </Col>
-                                    </Row>
+                                    </Row> */}
                                     <Row>
                                         <div className="update ml-auto mr-auto">
                                             <Button
                                                 color="success"
                                                 type="submit"
-                                                disabled={buyDisabled}
+                                                disabled={transferDisabled}
                                             >
-                                                Buy Bitcoin
-                                            </Button>
-                                        </div>
-                                    </Row>
-                                </Form>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md="12">
-                        <Card className="card-user">
-                            <CardHeader>
-                                <CardTitle tag="h5">Sell Transaction Form</CardTitle>
-                            </CardHeader>
-                            <CardBody>
-                                <Form onSubmit={handleSellSubmit}>
-                                    <Row>
-                                        <Col className="pr-1" md="3">
-                                            <FormGroup>
-                                                <label>Commission Type</label>
-                                                <Input type="select" name="commissionType" id="commissionType"
-                                                    value={sellCommissionType} onChange={e => setSellCommissionType(e.currentTarget.value)}>
-                                                    <option>USD</option>
-                                                    <option>BTC</option>
-                                                </Input>
-                                            </FormGroup>
-                                        </Col>
-                                        <Col className="px-1" md="3">
-                                            <FormGroup>
-                                                <label>Assign Trader</label>
-                                                <Input type="select" name="transactionType" id="transactionType"
-                                                    value={sellTrader} onChange={e => setSellTrader(e.currentTarget.value)}>
-                                                    {traderList.map((t) => (
-                                                        <option key={t.id} value={t.id}>{t.name}</option>
-                                                    ))}
-                                                </Input>
-                                            </FormGroup>
-                                        </Col>
-                                        <Col className="px-1" md="3">
-                                            <FormGroup>
-                                                <label htmlFor="exampleInputEmail1">
-                                                    Commission
-                                                </label>
-                                                <Input placeholder={sellCommissionType === "BTC" ? sellCommission + " \u20BF" : "$" + sellCommission} disabled />
-                                            </FormGroup>
-                                        </Col>
-                                        <Col className="pl-1" md="3">
-                                            <FormGroup>
-                                                <label htmlFor="exampleInputEmail1">
-                                                    Current Cost
-                                                </label>
-                                                <Input placeholder={sellCost} disabled />
-                                            </FormGroup>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col md="12">
-                                            <FormGroup className="mt-4 ml-3 mr-1">
-
-                                                <Slider
-                                                    disabled={false}
-                                                    defaultValue={0}
-                                                    aria-labelledby="discrete-slider-always"
-                                                    step={0.1}
-                                                    marks
-                                                    min={0}
-                                                    max={clientProperties.btcBalance}
-                                                    valueLabelDisplay="on"
-                                                    color="error"
-                                                    onChange={handleSellChange}
-                                                />
-                                            </FormGroup>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <div className="update ml-auto mr-auto">
-                                            <Button
-                                                color="danger"
-                                                type="submit"
-                                                disabled={sellDisabled}
-                                            >
-                                                Sell Bitcoin
+                                                Transfer Money
                                             </Button>
                                         </div>
                                     </Row>
@@ -340,4 +244,4 @@ function NewTransaction(props) {
     );
 }
 
-export default NewTransaction;
+export default NewTransfer;
