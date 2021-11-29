4812350ff3c76f7e8abc1889dfd5242aaeb6c8db
Ashish Pratap <26324100+alphayama@users.noreply.github.com>
2021-11-28 21:16:44 -0600

    code cleanup

--- frontend/src/views/NewTransfer.js
+++ frontend/src/views/NewTransfer.js
@@ -37,32 +37,12 @@ import axios from 'axios';
 
 function NewTransfer(props) {
     const [transferTrader, setTransferTrader] = useState("trader-1");
-    //const [sellTrader, setSellTrader] = useState("trader-2");
-    // const [buyCommissionType, setBuyCommissionType] = useState("USD");
-    // const [sellCommissionType, setSellCommissionType] = useState("USD");
-    // const [buyCommission, setBuyCommission] = useState("0");
-    // const [sellCommission, setSellCommission] = useState("0");
-    // const [buyCost, setBuyCost] = useState(0.0);
-    // const [sellCost, setSellCost] = useState(0.0);
     const [transferDisabled, setTransferDisabled] = useState(true);
-    // const [sellDisabled, setSellDisabled] = useState(true);
     const [transferAmount, setTransferAmount] = useState(0);
-    // const [sellAmount, setSellAmount] = useState(0);
-    // const [btcRate, setBtcRate] = useState(0.0)
     const [traderList, setTraderList] = useState([]);
     const [clientProperties, setClientProperties] = useState({});
 
     axios.defaults.withCredentials = true;
-
-    //   // Get current Bitcoin price
-    //   const getCurrentBTC = () => {
-    //     axios.get('http://localhost:5000/btc-rate')
-    //       .then(response => {
-    //         setBtcRate(response.data)
-    //       }).catch(error => {
-    //         console.log(error);
-    //       })
-    //   }
 
     // Get trader list
     const getTraderList = () => {
@@ -94,22 +74,6 @@ function NewTransfer(props) {
         getUserData();
     }, []);
 
-    //   // Get current bitcoin price every 10 seconds
-    //   useEffect(() => {
-    //     const interval = setInterval(() => {
-    //       getCurrentBTC();
-    //     }, 10000);
-    //     return () => clearInterval(interval);
-    //   }, []);
-
-    //   // Change other variables when BTC rate changes
-    //   useEffect(() => {
-    //     setBuyCommission(getCommission(btcRate, buyAmount, buyCommissionType, "silver"));
-    //     setSellCommission(getCommission(btcRate, sellAmount, sellCommissionType, "silver"));
-    //     setBuyCost(buyAmount > 0 ? btcRate * buyAmount + buyCommission : 0.00);
-    //     setSellCost(sellAmount > 0 ? btcRate * sellAmount - sellCommission : 0.00);
-    //   }, [btcRate, buyAmount, sellAmount]);
-
     // Handler functions for transfer slider
     const handleTransferChange = (e, val) => {
         setTransferAmount(val);
@@ -120,17 +84,6 @@ function NewTransfer(props) {
             setTransferDisabled(false);
         }
     }
-
-    // // Handler functions for sell slider
-    // const handleSellChange = (e, val) => {
-    //     setSellAmount(val);
-    //     if (val === 0) {
-    //         setSellDisabled(true);
-    //     }
-    //     else {
-    //         setSellDisabled(false);
-    //     }
-    // }
 
     // Buy/Sell Axios Request
     const transferSubmit = (data) => {
@@ -155,17 +108,6 @@ function NewTransfer(props) {
         };
         transferSubmit(data);
     }
-
-    // // Handler function for submitting sell
-    // const handleSellSubmit = (e) => {
-    //     const data = {
-    //         commission_type: sellCommissionType,
-    //         amount: sellAmount,
-    //         traderId: sellTrader,
-    //         action: "sell"
-    //     };
-    //     transactionSubmit(data);
-    // }
 
     return (
         <>
@@ -202,27 +144,7 @@ function NewTransfer(props) {
                                             />
                                             </FormGroup>
                                         </Col>
-                                        
                                     </Row>
-                                    {/* <Row>
-                                        <Col md="12">
-                                            <FormGroup className="mt-4 ml-3 mr-1">
-                                                <label>Choose amount: (Max. USD 20,000)</label>
-                                                <Slider
-                                                    disabled={false}
-                                                    defaultValue={0}
-                                                    aria-labelledby="discrete-slider-always"
-                                                    step={0.1}
-                                                    marks
-                                                    min={0}
-                                                    max={20000}
-                                                    valueLabelDisplay="on"
-                                                    color="success"
-                                                    onChange={handleTransferChange}
-                                                />
-                                            </FormGroup>
-                                        </Col>
-                                    </Row> */}
                                     <Row>
                                         <div className="update ml-auto mr-auto">
                                             <Button
