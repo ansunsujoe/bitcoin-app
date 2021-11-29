435ef9ff051f1bbbdbeab8c0c82e6fe720e0b5d2
Ashish Pratap <26324100+alphayama@users.noreply.github.com>
2021-11-29 10:44:43 -0600

    code cleanup

--- backend/app/transfers.py
+++ backend/app/transfers.py
@@ -10,15 +10,6 @@ import requests
 def index():
     return "Hello World!"
 
-# @app.route("/btc-rate")
-# def btc_balance():
-#     r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-#     try:
-#         data = r.json()
-#         return to_response(data.get("bpi").get("USD").get("rate_float"))
-#     except Exception:
-#         return "Could not reach CoinDesk API to get BTC price", 500
-
 # Add transfer
 @app.route("/transfers", methods=["GET"])
 def get_transfers():
@@ -48,50 +39,6 @@ def user_transfer(user_id):
         db.session.commit()
         return "Success"
 
-# # Buy transfers for user
-# @app.route("/users/<user_id>/transfers/buys", methods=["GET"])
-# def user_transfer_buys(user_id):
-#     result = db.session.query(
-#         Transfer, User
-#         ).filter(
-#             Transfer.client_id == user_id
-#         ).filter(
-#             User.user_id == Transfer.trader_id
-#         ).all()
-#     transfers = []
-#     for t, u in result:
-#         transfers.append({
-#             "time": t.date,
-#             "name": u.name,
-#             #"commission": t.commission_type,
-#             "status": t.status,
-#             "value": t.amount
-#         })
-#     return to_response(transfers)
-
-# # Sell transfers for user
-# @app.route("/users/<user_id>/transfers/sells", methods=["GET"])
-# def user_transfer_sells(user_id):
-#     result = db.session.query(
-#         Transfer, User
-#         ).filter(
-#             Transfer.action == "sell"
-#         ).filter(
-#             Transfer.client_id == user_id
-#         ).filter(
-#             User.user_id == Transfer.trader_id
-#         ).all()
-#     transfers = []
-#     for t, u in result:
-#         transfers.append({
-#             "time": t.date,
-#             "name": u.name,
-#             "commission": t.commission_type,
-#             "status": t.status,
-#             "value": t.amount
-#         })
-#     return to_response(transfers)
-
 # Buy transfers for trader
 @app.route("/users/traders/<trader_id>/transfers/buys", methods=["GET"])
 def trader_transfer_buys(trader_id):
@@ -112,29 +59,6 @@ def trader_transfer_buys(trader_id):
             "value": t.amount
         })
     return to_response(transfers)
-
-# # Sell transfers for trader
-# @app.route("/users/traders/<trader_id>/transfers/sells", methods=["GET"])
-# def trader_transfer_sells(trader_id):
-#     result = db.session.query(
-#         Transfer, User
-#         ).filter(
-#             Transfer.action == "sell"
-#         ).filter(
-#             Transfer.trader_id == trader_id
-#         ).filter(
-#             User.user_id == Transfer.client_id
-#         ).all()
-#     transfers = []
-#     for t, u in result:
-#         transfers.append({
-#             "time": t.date,
-#             "name": u.name,
-#             "commission": t.commission_type,
-#             "status": t.status,
-#             "value": t.amount
-#         })
-#     return to_response(transfers)
 
 # Delete transfers (trader cancels)
 @app.route("/transfers/<transfer_id>", methods=["DELETE"])
