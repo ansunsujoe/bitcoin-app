7b4bc192e59c6c37040f7748bf0f1cbb87d02c16
Ashish Pratap <26324100+alphayama@users.noreply.github.com>
2021-12-04 18:22:14 -0600

    change icons

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -90,7 +90,7 @@ var routes = [
   {
     path: "/new-transfer",
     name: "New Transfer",
-    icon: "nc-icon nc-cart-simple",
+    icon: "nc-icon nc-credit-card",
     component: NewTransfer,
     layout: "/admin",
     traderOnly: false,
@@ -110,7 +110,7 @@ var routes = [
   {
     path: "/approve-transfers",
     name: "Approve Transfers",
-    icon: "nc-icon nc-check-2",
+    icon: "nc-icon nc-paper",
     component: Trader_Approve,
     layout: "/admin",
     traderOnly: true,
