52f2837dd2dced05b9020d8ec4e5fc9b2bac6b84
Ashish Pratap <26324100+alphayama@users.noreply.github.com>
2021-12-05 11:43:43 -0600

    fix trader issues transfer

--- backend/app/transfers.py
+++ backend/app/transfers.py
@@ -77,7 +77,7 @@ def trader_transfer(trader_user_id):
         
         # Commit to database
         db.session.add(new_transfer)
-        transfer_client = db.session.query(Client).filter(Client.client_id == new_transfer.client_id).first()
+        transfer_client = db.session.query(Client).filter(Client.user_id == new_transfer.client_id).first()
         transfer_client.fiat_balance+=new_transfer.amount
         db.session.commit()
         return "Success"
@@ -121,6 +121,6 @@ def transfer_delete(transfer_id):
 def transfer_accept(transfer_id):
     transfer = db.session.query(Transfer).filter(Transfer.transfer_id == transfer_id).first()
     transfer.status = "Complete"
-    transfer_client = db.session.query(Client).filter(Client.client_id == transfer.client_id).first()
+    transfer_client = db.session.query(Client).filter(Client.user_id == transfer.client_id).first()
     transfer_client.fiat_balance+=transfer.amount
     db.session.commit()
